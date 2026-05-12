5890d1608850ca41345f40daa7e6e3d0198bde11
Sea <benzthanathorn15@gmail.com>
2026-05-12 17:15:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -93,7 +93,6 @@ div.stButton > button {
 st.markdown("""
 <div class="hero-header">
     <h1 class="hero-title">Lightweight Image classification for malaria detection using mobilenetv2</h1>
-    <p style="font-family:Rajdhani; color:#4da3ff; letter-spacing:2px; margin-top:10px;">NEURAL SCANNER V2</p>
 </div>
 """, unsafe_allow_html=True)
 
