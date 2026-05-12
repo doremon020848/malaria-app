f29c5abda7c019f9524bbb52cdef4b8f2c3eb89f
Sea <benzthanathorn15@gmail.com>
2026-05-12 21:52:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ div.stButton > button {
     color: #02060c !important;
     font-family: 'Orbitron', sans-serif !important;
     height: 3.5rem !important;
-    border-radius: 5px !important;
+    border-radius: 10ppx !important;
     border: none !important;
     font-size: 1.1rem !important;
     margin-top: 1rem;
