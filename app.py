859e1f4524a9f1c6a21e5bd99c6d79d36edb51c8
Sea <benzthanathorn15@gmail.com>
2026-05-13 13:07:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -169,7 +169,7 @@ if mode == "SAMPLES":
         files = [f for f in os.listdir(SAMPLE_DIR) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
         if files:
             st.markdown('<p style="font-family:\'Inter\', sans-serif; font-size:1rem; font-weight:500; color:#8892b0; margin-bottom:-10px;">CHOOSE DATASET:</p>', unsafe_allow_html=True)
-            choice = st.selectbox("CHOOSE DATASET:", files)
+            choice = st.selectbox(, files)
             if choice:
                 img = Image.open(os.path.join(SAMPLE_DIR, choice)).convert("RGB")
         else:
